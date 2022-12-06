9339b80f4b15ba9641c84cd8e9e16fd4c53dd36b
AidaFig <aidafigueiredo8@gmail.com>
2022-12-05 22:23:37 -0300

    feat: adding Car component in the Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import Logo  from '../../assets/logo.svg'
+import { Car } from '../../components/Car'
 import { Container, Header, TotalCars, HeaderContainer } from './styles';
 
 export function Home(){
@@ -23,6 +24,7 @@ export function Home(){
                     </TotalCars>
                 </HeaderContainer>
             </Header>
+            <Car />
         </Container>
     )
 }

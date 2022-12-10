30b13c8db025f06e99ba805fab69687f60c47815
AidaFig <aidafigueiredo8@gmail.com>
2022-12-09 21:25:17 -0300

    feat: adding new components to CarDetails screen

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -3,7 +3,19 @@ import React from 'react';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
 
-import { Container, Header } from './styles';
+import { 
+    Container, 
+    Header, 
+    Content, 
+    Details, 
+    Description, 
+    Brand, 
+    Name, 
+    Rent, 
+    Period, 
+    Price,
+    About 
+} from './styles';
 
 export const CarDetails = () => {
     return (
@@ -13,6 +25,25 @@ export const CarDetails = () => {
                 <BackButton onPress={() => {}}/>
             </Header>
             <ImageSlider imageUrl={['https://assets.stickpng.com/images/580b585b2edbce24c47b2c21.png']}/>
+
+            <Content>
+                <Details>
+                    <Description>
+                        <Brand>Lamborghini</Brand>
+                        <Name>Huracan</Name>
+                    </Description>
+                    <Rent>
+                        <Period>Ao dia</Period>
+                        <Price>R$ 580</Price>
+                    </Rent>
+                </Details>
+
+                <About>
+                    Este é um automóviel desportivo. Surgiu do lendário
+                     touro de lide indultadona praça Real Maestranza de Sevilla. 
+                     É um belíssimo carro pra quem gosta de acelerar.
+                </About>
+            </Content>
         </Container>
     )    
 }

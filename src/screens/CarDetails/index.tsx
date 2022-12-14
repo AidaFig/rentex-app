dc3c4424e3fa012bbaf486aaa8aa9cae2c8342e9
AidaFig <aidafigueiredo8@gmail.com>
2022-12-13 21:43:46 -0300

    feat: importing Accessoy in the CarDetails screen and importing svgs

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
-
+import { Accessory } from '../../components/Accessory';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+
+import speedSvg from '../../assets/speed.svg';
+import accelerationSvg from '../../assets/acceleration.svg';
+import forceSvg from '../../assets/force.svg';
+import gasolineSvg from '../../assets/gasoline.svg';
+import peopleSvg from '../../assets/people.svg';
+import exchangeSvg from '../../assets/exchange.svg';
 
 import { 
     Container, 
@@ -14,7 +21,8 @@ import {
     Rent, 
     Period, 
     Price,
-    About 
+    About,
+    Accessories
 } from './styles';
 
 export const CarDetails = () => {
@@ -38,11 +46,22 @@ export const CarDetails = () => {
                     </Rent>
                 </Details>
 
+                <Accessories>
+
+                    <Accessory name='380Km/h' icon={speedSvg}/>
+                    <Accessory name='3.2s' icon={accelerationSvg}/>
+                    <Accessory name='800HP' icon={forceSvg}/>
+                    <Accessory name='Gasolina' icon={gasolineSvg}/>
+                    <Accessory name='Auto' icon={exchangeSvg}/>
+                    <Accessory name='2 pessoas' icon={peopleSvg}/>
+                </Accessories>
+
                 <About>
-                    Este é um automóviel desportivo. Surgiu do lendário
-                     touro de lide indultadona praça Real Maestranza de Sevilla. 
+                    Este é um automóvel desportivo. Surgiu do lendário
+                     touro de lide indultado na praça Real Maestranza de Sevilla. 
                      É um belíssimo carro pra quem gosta de acelerar.
                 </About>
+
             </Content>
         </Container>
     )    
